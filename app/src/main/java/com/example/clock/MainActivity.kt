5fc09c2d3746c7ea2f07b5bceb6fef1e66ce0640
Swethaanura <swethae65@gmail.com>
2026-03-20 19:32:04 +0100

    test revoked

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivittty : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var timeText: TextView
     private val handler = Handler(Looper.getMainLooper())
